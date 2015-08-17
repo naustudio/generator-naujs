51b384061b7924a0df48f2f9c5ca39126ce9b248
Thanh Tran <trongthanh@gmail.com>
2015-08-17 15:18:42 +0700

    Adding HTML5 boilerplate with adaptation to use SCSS

---
+++ generators/app/templates/src/js/main.js
@@ -0,0 +1,9 @@
+/* © 2015 NauStud.io
+ * @author ...
+ */
+$(function() {
+	// For fun, displaying Nau ASCII Art:
+	var NAU_ASCII_ART = '   _.._             _.._           .--┐\n .`--.  \'.        .\'  .-`\'.       |   |\n.   .-\\   \\  (`) .   /-.   \\      |   |\n|   |  \\   \\     |   |  \\   \\     |   |\n|   |   \\   \\    |   |   \\   \\    |   |\n|   |    \\   \\   |   |    \\   \\   |   |\n|   |     \\   \\  |   |     \\   \\  |   |\n|   |      \\   `-/   |      \\   \\-\'   |\n|   |       \\_.-`   ,\'  (`)  \\   `-._.\'\n└--`         `-...-`          `-...-`\nNau Studio';
+	console.log(NAU_ASCII_ART);
+	$('body').append('<pre>' + NAU_ASCII_ART + '</pre>');
+})
