4bb1cfa2cd948292a86462e06962cbd23be6c03e
Thanh Tran <trongthanh@gmail.com>
2015-08-20 13:08:29 +0700

    Write up unit tests for iconfont

--- test/iconfont.js
+++ test/iconfont.js
@@ -5,16 +5,33 @@ var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 
 describe('Naujs:generators/iconfont', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/iconfont'))
-      .withArguments('name')
-      .withOptions({ skipInstall: true, force: true })
-      .on('end', done);
-  });
+	before(function (done) {
+		helpers.run(path.join(__dirname, '../generators/iconfont'))
+			.withOptions({ skipInstall: true, force: true })
+			.on('end', done);
+	});
 
-  // it('creates files', function () {
-  //   assert.file([
-  //     'somefile.js'
-  //   ]);
-  // });
+	it('creates files', function () {
+		assert.file([
+			'assets/iconfont-templates/_icons.scss',
+			'assets/iconfont-templates/icons.html',
+			'assets/icons/_README.md',
+			'gulpfile.js',
+			'package.json'
+		]);
+	});
+
+	it('sets package.json dependencies', function () {
+		assert.fileContent([
+			['package.json', 'gulp-iconfont'],
+			['package.json', 'gulp-consolidate'],
+			['package.json', 'lodash']
+		]);
+	});
+
+	it('injects iconfont task to gulpfile.js', function () {
+		assert.fileContent([
+			['gulpfile.js', /gulp\.task\('iconfont'/]
+		]);
+	});
 });
